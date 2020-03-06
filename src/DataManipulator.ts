c306880db57ff654c0c54b159a67115fb51e188f
Jervis <jerv0003@e.ntu.edu.sg>
2020-03-07 00:13:32 +0800

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_ABC: number,
+  price_DEF: number,
+  ratio:number,
   timestamp: Date,
+  upperbound: number,
+  lowerbound: number
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]): Row[] {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const price_ABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const price_DEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    const ratio = price_ABC/price_DEF;
+    const upperbound = 1 + 0.05;
+    const lowerbound = 1 - 0.05;
+    return {
+      price_ABC,
+      price_DEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upperbound,
+      lowerbound,
+      trigger_alert: (ratio>upperbound || ratio < lowerbound) ? ratio: undefined,
+    };
   }
 }
